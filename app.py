ae75a5d9b07014b0f8718b6186363801979c8b8b
Vivek Viswam <engineer.vivek.viswam@gmail.com>
2025-04-25 01:47:01 -0400

    Added sample query

--- app.py
+++ app.py
@@ -5,16 +5,26 @@ from sqlalchemy import create_engine, text
 
 st.set_page_config(page_title="Query Results Viewer", layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>AWS RDS Query Viewer</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>DMQL - Team 23</h1>", unsafe_allow_html=True)
 
 username = "postgres"
 password = "password"
 host = "database-1.cohgsoe8efzr.us-east-1.rds.amazonaws.com"
 port = "5432"
 database = "Retail"
+default_query = """SELECT c.Name, SUM(p.Amount) AS TotalSpent
+        FROM Payments p
+        JOIN Sales s ON
+        p.OrderNumber = s.OrderNumber
+        JOIN Customers c ON
+        s.CustomerKey = c.CustomerKey
+        GROUP BY c.Name
+        ORDER BY TotalSpent DESC
+        LIMIT 5;"""
+
 
 st.subheader("Enter Your SQL Query")
-query = st.text_area("SQL Query", height=180)
+query = st.text_area("SQL Query", value=default_query, height=250)
 
 if st.button("Run Query"):
     try:
